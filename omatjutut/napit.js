364f434599377e4e9edcf866ab6faad1d09bd23f
jennatuomikoski <jenna.tuomikoski@edu.keuda.fi>
2023-07-28 17:40:50 +0300

    rontille

---
+++ omatjutut/napit.js
@@ -0,0 +1,9 @@
+function sininen(sin) {
+    sin.style.backgroundColor = "blue";
+}
+function punanen(pun) {
+    pun.style.backgroundColor = "red";
+}
+function musta(mus) {
+    mus.style.backgroundColor = "black";
+}
