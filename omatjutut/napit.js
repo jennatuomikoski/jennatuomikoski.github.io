87062999c844857956ae5faefd840102ae608bd5
jennatuomikoski <111268813+jennatuomikoski@users.noreply.github.com>
2026-02-24 13:35:44 +0200

    Delete omatjutut directory

--- omatjutut/napit.js
+++
@@ -1,9 +0,0 @@
-function nappi(btn) {
-    if (btn.id === "sini") {
-        document.body.style.backgroundColor = "blue";
-    } else if (btn.id === "puna") {
-        document.body.style.backgroundColor = "red";
-    } else if (btn.id === "musta") {
-        document.body.style.backgroundColor = "black";
-    } 
-}
