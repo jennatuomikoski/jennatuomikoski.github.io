4101f3650cb8a9511f3cd1d71505e33b0f3cc048
jennatuomikoski <111268813+jennatuomikoski@users.noreply.github.com>
2026-02-24 13:35:07 +0200

    Delete demo6 directory

--- demo6/kirjautuminen.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", onkoKirjautunut);
-
-function onkoKirjautunut() {
-    let kirjautunut = 'kyllä';
-    if (kirjautunut === 'kyllä') {
-        document.getElementById('tervetulo_teksti').textContent += ' Jenu!'
-    }
-}
